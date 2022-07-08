316a0316672dddc6a38b43f4056f7f3218f71b09
KaiStryker <kareemj.williams@gmail.com>
2022-07-08 00:19:34 -0400

    updated contract

--- contracts/txOlympus.sol
+++ contracts/txOlympus.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 
 contract txOlympus is Ownable {
